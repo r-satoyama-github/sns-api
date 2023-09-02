872b52c19a2813f8a4f934c28b095166ed5a1cb3
ryoichi <ryoichi.satoyama@gmail.com>
2023-09-02 15:47:41 +0900

    upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const authRoute = require("./routers/auth");
+require("dotenv").config();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
