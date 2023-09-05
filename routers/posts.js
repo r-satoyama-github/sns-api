c9c26839ad165891e2d60ecc4dcef8928ee26cc9
ryoichi <ryoichi.satoyama@gmail.com>
2023-09-05 22:53:16 +0900

    done login,logout,posts

---
+++ routers/posts.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
+const prisma = new PrismaClient();
+
+// 呟き投稿用API
+router.post("/post", isAuthenticated, async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    return res.status(400).json({ message: "投稿内容がありません。" });
+  }
+
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        authorId: req.userId,
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            username: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "サーバーエラーです。" });
+  }
+});
+
+// 最新つぶやき取得用API
+router.get("/latest_posts", async (req, res) => {
+  try {
+    const latestPosts = await prisma.post.findMany({
+      take: 10,
+      orderBy: { createdAt: "desc" },
+      include: {
+        author: {
+          select: {
+            id: true,
+            username: true,
+            email: true,
+          },
+        },
+      },
+    });
+    res.json(latestPosts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "サーバーエラーです。" });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const postsRoute = require("./routers/posts");
+const userRoute = require("./routers/user");
+const cors = require("cors");
 require("dotenv").config();
 
 const PORT = 8081;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/user", userRoute);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
